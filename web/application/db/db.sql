6037bbf27a05ba45681730709dd1da4a6e5d25ed
Paul Draper <paulddraper@gmail.com>
2013-04-04 23:39:34 -0600

    Fix previous commit

---
+++ web/application/db/db.sql
@@ -0,0 +1,16 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE user (
+  username TEXT PRIMARY KEY
+  , currentclueid INTEGER REFERENCES clue(id)
+);
+
+CREATE TABLE clue (
+  id INTEGER PRIMARY KEY
+  , lat REAL NOT NULL
+  , lng REAL NOT NULL
+  , clue TEXT NOT NULL
+  , question TEXT NOT NULL
+  , answer TEXT NOT NULL
+  , nextclueid INTEGER REFERENCES clue(id)
+);
