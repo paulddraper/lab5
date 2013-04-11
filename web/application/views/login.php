222f5ccb2d9c3308a3b0ce814512c966238a021c
Paul Draper <paulddraper@gmail.com>
2013-04-11 13:19:41 -0600

    Change login page

---
+++ web/application/views/login.php
@@ -0,0 +1,7 @@
+<div class="container">
+	<!-- <a href="<?=base_url()?>">back</a> --><h1>Login</h1>
+	<form action="<?=base_url()?>app/start" method="post">
+		<input name="phone" placeholder="phone"><br>
+		<button type="submit">Start!</button><br>
+	</form>
+</div>
